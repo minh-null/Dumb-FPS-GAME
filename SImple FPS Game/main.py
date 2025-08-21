e4d20567984ca7c10a467aa27548db2e4a5151e1
Minh <124509308+minh-null@users.noreply.github.com>
2025-08-21 10:34:35 +0700

    some stupid update (debug)
    still, the animation stopped working :((

--- SImple FPS Game/main.py
+++ SImple FPS Game/main.py
@@ -1,33 +1,7 @@
 from ursina import *
 from ursina.shaders import lit_with_shadows_shader
 from ursina.prefabs.first_person_controller import FirstPersonController
-
-import os
-import sys
 from random import uniform
-
-if getattr(sys, "frozen", False):
-    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
-else:
-    base_path = os.path.dirname(os.path.abspath(__file__))
-
-
-def update():
-    #frame
-    if held_keys['escape']:
-        application.quit()
-    
-    #Movement thing :33
-    if held_keys['control']:
-        player.speed = 10
-    else:
-        player.speed = 5
-        
-    if held_keys["space"]:
-        player.jump_height = 5
-    
-    if mouse.left:
-        shoot()
 
 app = Ursina()
 
@@ -37,64 +11,74 @@ ground = Entity(
     collider='box', 
     scale=(50,1,50))
 
-#=========================Objekt and Maps============================
 wall = Entity(model='cube', color=color.azure, scale=(2,2,2), position=(5,1,5), collider='box')
 wall2 = Entity(model='cube', color=color.orange, scale=(2,3,1), position=(-3,1.5,2), collider='box')
+tree = Entity(model='tree.obj', texture='tree.png', scale=2, position=(3,0,3), collider='box')
 
-tree = Entity(
-    model='tree.obj',
-    texture='tree.png', 
-    scale=2,
-    position=(3,0,3),
-    collider='box'
-)
+player = FirstPersonController()
 
+gun_holder = Entity(parent=camera, position=(1,-0.75,1.5))
 gun = Entity(
-    parent=camera,
+    parent=gun_holder,
     model='Glock17.obj',
     scale=0.2,
-    position=(1, -0.75, 1.5),
-    rotation=(5, -180, 0),
+    rotation=(5,-180,0),
     shader=lit_with_shadows_shader,
     normal_map='Glock17_Normal.png',
     roughness_map='Glock17_Roughness.png',
     metallic_map='Glock17_Metalness.png'
 )
 
-gun.always_on_top = True
+gun_origin_pos = gun_holder.position
+gun_origin_rot = gun_holder.rotation
 
-gun_origin_pos = gun.position
-gun_origin_rot = gun.rotation
+fire_rate = 0.15
+time_since_last_shot = 0
 
 def shoot():
-    random_yaw = uniform(-2, 2)   # yaw <-->
-    random_pitch = uniform(3, 6)  # pitch up-down
+    random_yaw = uniform(-4, 4)    
+    random_pitch = uniform(10, 15)  
 
-    gun.animate_rotation((gun_origin_rot[0] + random_pitch,
-                          gun_origin_rot[1] + random_yaw,
-                          gun_origin_rot[2]),
-                         duration=0.05, curve=curve.out_expo)
+    gun_holder.animate_rotation(
+        Vec3(gun_origin_rot.x + random_pitch,
+             gun_origin_rot.y + random_yaw,
+             gun_origin_rot.z),
+        duration=0.08, curve=curve.out_expo
+    )
+    gun_holder.animate_rotation(gun_origin_rot, duration=0.2, delay=0.1, curve=curve.in_out_quint)
 
-    gun.animate_rotation(gun_origin_rot,
-                         duration=0.2, delay=0.05, curve=curve.in_out_quint)
+    gun_holder.animate_position(
+        Vec3(gun_origin_pos.x, gun_origin_pos.y, gun_origin_pos.z - 0.7),
+        duration=0.05, curve=curve.out_expo
+    )
+    gun_holder.animate_position(gun_origin_pos, duration=0.2, delay=0.1, curve=curve.in_out_quint)
 
-    gun.animate_position((gun_origin_pos[0],
-                          gun_origin_pos[1],
-                          gun_origin_pos[2] - 0.2),
-                         duration=0.05, curve=curve.out_expo)
+    print("Bang!")
 
-    gun.animate_position(gun_origin_pos,
-                         duration=0.2, delay=0.05, curve=curve.in_out_quint)
+def update():
+    global time_since_last_shot
+    time_since_last_shot += time.dt
+
+    if held_keys['escape']:
+        print("player press _escape_" )
+        application.quit()
+
+    player.speed = 5
     
-    
+    if held_keys['control']:
+        player.speed = 7
+        print("player press _control_" )
+    if held_keys["space"]:
+        player.jump_height = 3
+        print("player press _space_" )
+        if held_keys['control']:
+            player.jump_height = 5
+            print("player press _space_ & _control_" )
 
-#====================================================================
 
-
-#Light
-DirectionalLight().look_at(Vec3(1,-1,-1))
-
-#player
-player = FirstPersonController()
+def input(key):
+    if key == 'left mouse down':
+        print("player press _left-mouse-button_" )
+        shoot()
 
 app.run()
