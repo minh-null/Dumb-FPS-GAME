d3dc9dd861ee42a4b69dbba1b1036f4512f73502
Minh <124509308+minh-null@users.noreply.github.com>
2025-08-23 21:46:20 +0700

    Added debug abilities

--- SImple FPS Game/main.py
+++ SImple FPS Game/main.py
@@ -2,6 +2,20 @@ from ursina import *
 from ursina.shaders import lit_with_shadows_shader
 from ursina.prefabs.first_person_controller import FirstPersonController
 from random import uniform
+import pygame
+from json import load, dump
+from enum import Enum, auto, IntEnum
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Optional, Any
+import networkx as nx
+import datetime
+
+if getattr(sys, "frozen", False): #Rip som ;-; (atleast its python version)
+    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+else:
+    base_path = os.path.dirname(os.path.abspath(__file__))
 
 app = Ursina()
 
@@ -36,45 +50,40 @@ fire_rate = 0.15
 time_since_last_shot = 0
 
 def shoot():
-    random_yaw = uniform(-4, 4)    
-    random_pitch = uniform(10, 15)  
-
-    gun_holder.animate_rotation(
-        Vec3(gun_origin_rot.x + random_pitch,
-             gun_origin_rot.y + random_yaw,
-             gun_origin_rot.z),
-        duration=0.08, curve=curve.out_expo
-    )
-    gun_holder.animate_rotation(gun_origin_rot, duration=0.2, delay=0.1, curve=curve.in_out_quint)
-
-    gun_holder.animate_position(
-        Vec3(gun_origin_pos.x, gun_origin_pos.y, gun_origin_pos.z - 0.7),
-        duration=0.05, curve=curve.out_expo
-    )
-    gun_holder.animate_position(gun_origin_pos, duration=0.2, delay=0.1, curve=curve.in_out_quint)
-
-    print("Bang!")
-
+    time = datetime.datetime.now().strftime("%H-%M-%S")
+    print(str(time), "Bang!")
+    bullet = Entity(model='cube', color=color.azure, scale=(2,2,2), collider='box')
+    bullet_origin_pos = (5, 10, 5)
+    
 def update():
-    global time_since_last_shot
-    time_since_last_shot += time.dt
-
+    time = datetime.datetime.now().strftime("%H-%M-%S")
     if held_keys['escape']:
-        print("player press _escape_" )
+        print(str(time), "player press _escape_")
         application.quit()
 
     player.speed = 5
-    
+    if held_keys['w']:
+        print(str(time), "player press _w_" )
+        
+    if held_keys['a']:
+        print(str(time), "player press _a_" )
+        
+    if held_keys['s']:
+        print(str(time), "player press _s_" )
+        
+    if held_keys['d']:
+        print(str(time), "player press _d_" )
+        
     if held_keys['control']:
         player.speed = 7
-        print("player press _control_" )
+        print(str(time), "player press _control_" )
+        
     if held_keys["space"]:
         player.jump_height = 3
-        print("player press _space_" )
+        print(str(time), "player press _space_" )
         if held_keys['control']:
             player.jump_height = 5
-            print("player press _space_ & _control_" )
-
+            print(str(time), "player press _space_ & _control_" )
 
 def input(key):
     if key == 'left mouse down':
